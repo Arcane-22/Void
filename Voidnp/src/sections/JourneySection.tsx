acfc509cc522d1a18ea200823ad5048e39857e64
Arcane-22 <siwakotisuchit22@gmail.com>
2026-05-08 15:50:14 +0545

    fixed css values modified

--- Voidnp/src/sections/JourneySection.tsx
+++ Voidnp/src/sections/JourneySection.tsx
@@ -7,7 +7,9 @@ function JourneySection() {
       onMouseEnter={e => e.currentTarget.classList.add('hovered')}
       onMouseLeave={e => e.currentTarget.classList.remove('hovered')}
     >
+      <div className="journey-text">
         <h2 className="journey-heading">Your Journery To Verification<span className='dot'>.</span></h2>
+      </div>
       <article>
         <JourneryCardList/>
       </article>
